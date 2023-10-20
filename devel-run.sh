443596f8ca5ebef65cbbf807e2c1d187b171c7f8
Ales Krenek <ljocha@ics.muni.cz>
2023-10-20 18:04:40 +0000

    it does something

--- devel-run.sh
+++ devel-run.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-image=ljocha/ptltsne
+true ${image:=ljocha/ptltsne}
+
 cmd=parmtSNEcv
-if [ $(basename $0) = run-tune.sh ]; then
-	cmd = tune/tune.py
+gpuflags="--gpus all"
+
+if [ $(basename $0) = tune-run.sh ]; then
+	cmd=tune/tune.py
+elif [ $(basename $0) = tune-run-amd.sh ]; then
+	cmd=tune/tune.py
+	image=$image:amd
+	gpuflags="--device=/dev/kfd --device=/dev/dri --group-add=video --group-add=render"
+elif [ $(basename $0) = devel-run-amd.sh ]; then
+	image=$image:amd
+	gpuflags="--device=/dev/kfd --device=/dev/dri --group-add=video --group-add=render"
 fi
 
 docker run \
-	--gpus all \
 	--ipc=host --ulimit memlock=-1 --ulimit stack=67108864 \
 	-u $(id -u) -ti -v $PWD:/work -w /work \
-	$image $cmd "$@"
+	$gpuflags $image $cmd "$@"
 
--- tune/tune.py
+++ tune/tune.py
@@ -6,26 +6,37 @@ import numpy as np
 
 args = p.parse_args()
 adict = p.process_args(args)
+adict2 = { **adict }
 
 
 def tuned(config):
-  params = adict.copy()
+  params = adict2.copy()
   for k in params:
     if k in config:
       params[k] = config[k]
 
-  cvs = p.parmtSNEcollectivevariable(**params)
-	d = np.linalg.norm(cvs[14]-cvs[815])
-	return { 'pairdist' : d }
+# XXX:
+  if 'actfun2' not in config: params['actfun2'] = params['actfun1']
+  if 'actfun3' not in config: params['actfun3'] = params['actfun1']
+  if 'layer2' not in config: params['layer2'] = params['layer1']
+  if 'layer3' not in config: params['layer3'] = params['layer1']
+
+  cvs = p.parmtSNEcollectivevariable(**params).to('cpu').detach().numpy()
+  d = np.linalg.norm(cvs[14]-cvs[815])
+  return { 'pairdist' : d }
+
+def test(config):
+  return { 'score', 1 }
 
 
 space = {
   'layers': tune.choice([1,2,3]),
-	'layer1': tune.choice([32,64,128,256]),
-	'layer2': tune.sample_from(lambda x: x.config.layer1),
-	'layer3': tune.sample_from(lambda x: x.config.layer1),
+  'layer1': tune.choice([32,64,128,256]),
+  'actfun1': tune.choice(['tanh','sigmoid']),
 }
 
-tuner = tune.Tuner(tuned, param_space = space)
+
+tuner = tune.Tuner(tuned, param_space=space)
+#tuner = tune.Tuner(test, param_space=space)
 results = tuner.fit()
-print(results.get_best_result(metric='pairdist','max').config)
+print(results.get_best_result(metric='pairdist',mode='max').config)
